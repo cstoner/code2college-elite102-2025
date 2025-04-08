f77bc0c61652b09f55db40820b27180cc005bf9f
Cye Stoner <stonerc@gmail.com>
2025-04-08 18:15:06 -0500

    Added mysql connection info

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #!/bin/env python3
+
+import mysql.connector
+
+connection = mysql.connector.connect(user = 'root', database = 'pokemon', password = 'password')
+
+cursor = connection.cursor()
+
+testQuery = ("SELECT * FROM pokemon")
+
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item) 
+
+cursor.close()
+
+connection.close()
